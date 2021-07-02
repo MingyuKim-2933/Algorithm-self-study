310c140b1e5e71a239c6a892a145755b754b90e8
Mingyu Kim <68273065+MingyuKim-2933@users.noreply.github.com>
2021-07-02 13:47:27 +0900

    Update 완주하지 못한 선수.py

--- Programmers/완주하지 못한 선수.py
+++ Programmers/완주하지 못한 선수.py
@@ -10,7 +10,7 @@ completion의 길이는 participant의 길이보다 1 작습니다.
 참가자의 이름은 1개 이상 20개 이하의 알파벳 소문자로 이루어져 있습니다.
 참가자 중에는 동명이인이 있을 수 있습니다.
 
-<입출력 예>
+<입출력 예
 participant                                             completion	                                return
 ["leo", "kiki", "eden"]                                 ["eden", "kiki"]	                        "leo"
 ["marina", "josipa", "nikola", "vinko", "filipa"]       ["josipa", "filipa", "marina", "nikola"]    "vinko"
