c58b5e53ad8ee5a993311bc6b5fec6dbff1a261c
Mingyu Kim <68273065+MingyuKim-2933@users.noreply.github.com>
2021-08-02 17:20:20 +0900

    Update 음양 더하기.py

--- Programmers/코딩테스트 연습/모든 문제/음양 더하기.py
+++ Programmers/코딩테스트 연습/모든 문제/음양 더하기.py
@@ -11,8 +11,8 @@ signs[i] 가 참이면 absolutes[i] 의 실제 정수가 양수임을, 그렇지
 
 입출력 예
 absolutes	  signs	                result
-[4,7,12]	  [true,false,true]	    9
-[1,2,3]	    [false,false,true]	  0
+[4,7,12]	  [true,false,true]	9
+[1,2,3]	            [false,false,true]	  0
 
 입출력 예 설명
 
