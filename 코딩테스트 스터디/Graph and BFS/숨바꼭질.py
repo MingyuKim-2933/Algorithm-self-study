1b9b38a73517f5501729ba67333ad5ae3ea73429
MINGYU <9832933k@hanmail.net>
2021-12-14 06:29:18 +0900

    Update 숨바꼭질.py

--- 코딩테스트 스터디/Graph and BFS/숨바꼭질.py
+++ 코딩테스트 스터디/Graph and BFS/숨바꼭질.py
@@ -10,6 +10,6 @@ while q:
         print(dist[x])
         break
     for nx in (x-1, x+1, x*2):
-        if 0 <=nx <= 100000 and not dist[nx]:
+        if 0 <= nx <= 100000 and not dist[nx]:  # dist[nx]가 0일 때(처음 방문했을 때)만 계산한다.
             dist[nx] = dist[x] + 1
             q.append(nx)
